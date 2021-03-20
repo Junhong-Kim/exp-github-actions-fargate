4c82862e48953e6013f6b269b2df6bba801764a4
Junhong-Kim <jhk1005jhk@gmail.com>
2021-03-21 02:31:11 +0900

    HealthCheck API 및 Dockerfile 추가

---
+++ src/main/java/com/example/githubactions/WebController.java
@@ -0,0 +1,19 @@
+package com.example.githubactions;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class WebController {
+
+    @Value("${sample.value}")
+    private String value;
+
+    @GetMapping("/healthCheck")
+    public String healthCheck() {
+        return String.format("OK %s", value);
+    }
+}
